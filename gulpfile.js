9aa49c8b25079961b3ac74a302d263db8ee7d27f
前田凌汰 <>
2020-05-29 14:41:45 +0900

    途中経過

--- gulpfile.js
+++ gulpfile.js
@@ -27,5 +27,13 @@ gulp.task('sass', function () {
     })) // 取得したscssをcssに変換
     .pipe(gulp.dest('css')) // 変換した結果をcssフォルダに出力
   )
+})
+
+// sassファイルをずっと監視するタスク
+gulp.task('sass:watch', function () {
+
+  // sassフォルダ内の全scssファイルの変更を監視
+  // 変更があったら、scssをcssに変換
+  gulp.watch('sass/*.scss', gulp.task('sass'))
 
 })
