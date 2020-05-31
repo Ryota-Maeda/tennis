34d65f2efd135c40d3df1f188a6c407400d0a380
Atsushi_Yoshino <atsushi.yoshino.0705@gmail.com>
2020-06-01 08:35:56 +0900

    2,3ページ目完成

---
+++ js/app3.js
@@ -0,0 +1,108 @@
+$('.drawer').drawer({
+    class: {
+      nav: 'drawer-nav',
+      toggle: 'drawer-toggle',
+      overlay: 'drawer-overlay',
+      open: 'drawer-open',
+      close: 'drawer-close',
+      dropdown: 'drawer-dropdown'
+    },
+    iscroll: {
+      // Configuring the iScroll
+      // https://github.com/cubiq/iscroll#configuring-the-iscroll
+      mouseWheel: true,
+      preventDefault: false
+    },
+    showOverlay: true
+  });
+  
+function getParameter(){
+	//Return
+	var ret=null;
+	//URL取得
+  var url = location.href; 
+  
+  console.log(url);
+
+	//URL分割
+	parameters = url.split("?");
+	//パラメータあり
+	if( parameters.length > 1 ) {
+		//分割
+    var params   = parameters[1].split("&");
+    
+		//パラメータ配列
+		var paramsArray = [];
+		//パラメータ数繰り返し
+		for ( i = 0; i < params.length; i++ ) {
+       var neet = params[i].split("=");
+       
+		   paramsArray.push(neet[0]);
+		   paramsArray[neet[0]] = neet[1];
+		}
+		//Get Param
+		ret = paramsArray["player"];
+	}
+    //
+    return ret;
+};
+
+let player = getParameter();
+
+let slideIndex = 0;
+
+if (player == 'federer') {
+  slideIndex = 3;
+} else if (player == 'nadal') {
+  slideIndex = 2;
+} else if (player == 'novak') {
+  slideIndex = 1;
+} else if (player == 'murray') {
+  slideIndex = 4;
+}
+
+
+var sliderSelector = '.swiper-container',
+    options = {
+      initialSlide: slideIndex,
+      init: false,
+      loop: true,
+      speed:800,
+      slidesPerView: 2, // or 'auto'
+      // spaceBetween: 10,
+      centeredSlides : true,
+      effect: 'coverflow', // 'cube', 'fade', 'coverflow',
+      coverflowEffect: {
+        rotate: 50, // Slide rotate in degrees
+        stretch: 0, // Stretch space between slides (in px)
+        depth: 100, // Depth offset in px (slides translate in Z axis)
+        modifier: 1, // Effect multipler
+        slideShadows : true, // Enables slides shadows
+      },
+      grabCursor: true,
+      parallax: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        1023: {
+          slidesPerView: 1,
+          spaceBetween: 0
+        }
+      },
+      // Events
+      on: {
+        imagesReady: function(){
+          this.el.classList.remove('loading');
+        }
+      }
+    };
+var mySwiper = new Swiper(sliderSelector, options);
+
+// Initialize slider
+mySwiper.init();
